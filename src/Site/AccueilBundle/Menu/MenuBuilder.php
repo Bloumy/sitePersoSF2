c9e6ceba505e025a74b9ac5dc93404d7b29bc5e1
Alexis Horde <alexis.horde@ign.fr>
2015-01-22 10:03:01 +0100

    Ajout d'un css global et taille du global container

--- src/Site/AccueilBundle/Menu/MenuBuilder.php
+++ src/Site/AccueilBundle/Menu/MenuBuilder.php
@@ -20,10 +20,16 @@ class MenuBuilder extends \Symfony\Component\DependencyInjection\ContainerAware 
 
         $bundlename = $request->get('_template')->get('bundle');
         $menu = $this->factory->createItem('root');
-      
-        $menu->addChild('Accueil', array('route' => 'site_home'))->setCurrent($bundlename == 'SiteAccueilBundle');
-        $menu->addChild('Contact', array('route' => 'contact'))->setCurrent($bundlename == 'SiteContactBundle');
-        $menu->addChild('Projets', array('route' => 'fiche_contact'))->setCurrent($bundlename == 'SiteProjetsBundle');
+
+        $menu->addChild(' Accueil', array('route' => 'site_home'))
+                ->setLinkAttribute('class', 'glyphicon glyphicon-home')
+                ->setCurrent($bundlename == 'SiteAccueilBundle');
+        $menu->addChild(' Contact', array('route' => 'contact'))
+                ->setLinkAttribute('class', 'glyphicon glyphicon-envelope')
+                ->setCurrent($bundlename == 'SiteContactBundle');
+        $menu->addChild(' Projets', array('route' => 'fiche_contact'))
+                ->setLinkAttribute('class', 'glyphicon glyphicon-book')
+                ->setCurrent($bundlename == 'SiteProjetsBundle');
 
 
         return $menu;
