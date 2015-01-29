87d9d3f09d48e8a547eef23954afaaa42de97a3d
Alexis Hordé <alexis.horde@gmail.com>
2015-01-29 20:07:50 +0100

    ajout d'un background

--- src/Site/ProjectsBundle/Resources/public/js/game_1.js
+++ src/Site/ProjectsBundle/Resources/public/js/game_1.js
@@ -1,3 +1,5 @@
+
+
 var context2D = null;
 var context3D = null;
 
@@ -10,11 +12,23 @@ function initContext(dimention) {
 }
 ;
 
+function customiseContext2D(context2D) {
+//    context2D.strokeStyle = "#0000FF";
+//    context2D.rect(200, 200, 600, 800); // x, y, width, height
+//    context2D.stroke();
 
+    var imagePanoramique;
+    var imagePanoramiqueX = 0;
+    var imagePanoramiqueY = 0;
 
-$(document).ready(function() {
+    imagePanoramique = new Image();
+    imagePanoramique.src = sourceImagePanoramique;
+    context2D.drawImage(imagePanoramique, imagePanoramiqueX, imagePanoramiqueY);
+}
+
+$(document).ready(function () {
     context2D = initContext('2d');
-    context3D = initContext('3d');
+
 });
 //
 ////http://www.faire-des-jeux.com/html5-tutorial-canvas/
